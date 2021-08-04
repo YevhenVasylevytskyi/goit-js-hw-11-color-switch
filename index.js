31fd01a9446e511bea46729977bc57037ac60690
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-08-04 12:31:27 +0300

    copmlete

--- index.js
+++ index.js
@@ -6,57 +6,39 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
 let timerId = null;
 
-// const min = 1;
-// const max = colors.length;
 const refs = {
     body: document.querySelector('.body'),
     startBtn: document.querySelector('[data-action="start"]'),
     stopBtn: document.querySelector('[data-action="stop"]'),
-}
+};
 
 refs.startBtn.addEventListener('click', onStart);
 refs.stopBtn.addEventListener('click', onStop);
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-function onStop() {
-    console.log("Клик на Стоп");
+function randomBackgroundColor() {
+    return colors[randomIntegerFromInterval(0, colors.length)]
+};
+
+function onStop() {    
     clearInterval(timerId);
+    refs.startBtn.disabled = false;
 };
 
 function onStart() {
-    timerId = setInterval(() => {
-        console.log("Клик на Старт")
-        // body.style.backgroundColor = colors[randomIntegerFromInterval];
-    
+    refs.startBtn.disabled = true;
+    timerId = setInterval(() => {        
+        refs.body.style.backgroundColor = randomBackgroundColor();
     }, 1000);    
 };
 
 
-
-// const min = 1;
-// const max = colors.length;
-    
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// // };
-
-// randomIntegerFromInterval(1, colors.length);
-// const randomColor = () => {
-//     return colors[randomIntegerFromInterval(min, max)];
-// };
-
-// console.log(randomColor);
-// const randomColors = colors[randomIntegerFromInterval(min, max)];
-// console.log(randomColors);
-// body.style.backgroundColor = colors[randomIntegerFromInterval];
-
-
-// stop.addEventListener('click', clickStop);
    
 
 
